11dcafba0f605ecf852f236510c2df7c20823ef0
ziauddin-0987 <ziauddink100@gmail.com>
2024-02-25 16:24:59 +0500

    this my js 'if ,else if, else'

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const email = prompt("please your inter email");
-const password = prompt("please your inter password");
-// const suffh = prompt("conputer insetute name");
+// const email = prompt("please your inter email");
+// const password = prompt("please your inter password");
+// // const suffh = prompt("conputer insetute name");
 
-if (email == "ziauddink100@gmail.com" && password == "abc") {
-  console.log("perfect you email nad password");
-} else if (email == "ziauddin" && password == "khan") {
-  console.log("not perfect is passworw and email");
-  // document.write("perfect you email")
-} else console.log("vaalid is not value");
+// if (email == "ziauddink100@gmail.com" && password == "abc") {
+//   console.log("perfect you email nad password");
+// } else if (email == "ziauddin" && password == "khan") {
+//   console.log("not perfect is passworw and email");
+//   // document.write("perfect you email")
+// } else console.log("vaalid is not value");
+let screenlight = prompt("chouse your screen color");
+if (screenlight == "white") {
+  console.log("display is white");
+} else if (screenlight == "black") {
+  console.log("display color black");
+} else if (screenlight == "green") {
+  console.log("display color green");
+} else if (screenlight == "red") {
+  console.log("display color red");
+} else {
+  console.log("unvalide is value");
+}
