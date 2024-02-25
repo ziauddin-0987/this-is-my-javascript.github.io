189c8ef90fb0848b4b5f4218b1eb8ebb1e860164
ziauddin-0987 <ziauddink100@gmail.com>
2024-02-25 16:27:15 +0500

    this is my js all code

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-// const email = prompt("please your inter email");
-// const password = prompt("please your inter password");
-// // const suffh = prompt("conputer insetute name");
+const email = prompt("please your inter email");
+const password = prompt("please your inter password");
+// const suffh = prompt("conputer insetute name");
 
-// if (email == "ziauddink100@gmail.com" && password == "abc") {
-//   console.log("perfect you email nad password");
-// } else if (email == "ziauddin" && password == "khan") {
-//   console.log("not perfect is passworw and email");
-//   // document.write("perfect you email")
-// } else console.log("vaalid is not value");
+if (email == "ziauddink100@gmail.com" && password == "abc") {
+  console.log("perfect you email nad password");
+} else if (email == "ziauddin" && password == "khan") {
+  console.log("not perfect is passworw and email");
+  // document.write("perfect you email")
+} else console.log("vaalid is not value");
 let screenlight = prompt("chouse your screen color");
 if (screenlight == "white") {
   console.log("display is white");
