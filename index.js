c6966cfe160122efa195a36cf0ba9b79f05de5a6
ziauddin-0987 <ziauddink100@gmail.com>
2024-02-25 16:09:43 +0500

    this is code if , else if , else

---
+++ index.js
@@ -0,0 +1,10 @@
+const email = prompt("please your inter email");
+const password = prompt("please your inter password");
+// const suffh = prompt("conputer insetute name");
+
+if (email == "ziauddink100@gmail.com" && password == "abc") {
+  console.log("perfect you email nad password");
+} else if (email == "ziauddin" && password == "khan") {
+  console.log("not perfect is passworw and email");
+  // document.write("perfect you email")
+} else console.log("vaalid is not value");
--- script.js
+++
@@ -1,6 +0,0 @@
-let a = parseInt(prompt("what is price"));
-let b = parseInt(prompt("how mach cheken gram"));
-
-let c = a / 1000;
-let d = c * b;
-document.write(`you have to pay ${d}`);
