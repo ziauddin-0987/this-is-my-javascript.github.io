7ffe230c4c48749dd55c3c6f22d9494326c1635e
ziauddin-0987 <ziauddink100@gmail.com>
2024-02-29 00:12:29 +0500

    this my js code in github

---
+++ script.js
@@ -0,0 +1,6 @@
+alert("what is cheiken price ");
+let divide = prompt("rupes 1 kg");
+let user = prompt("how much price is cheiken");
+let khan = user % divide;
+let zia = divide * khan;
+document.write(zia);
