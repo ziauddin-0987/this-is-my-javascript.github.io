d4d053ee05a11165dc3f6b6a6db032b26e6b67f8
ziauddin-0987 <ziauddink100@gmail.com>
2024-03-01 20:59:50 +0500

    the commit is array

---
+++ my_file-in.js
@@ -0,0 +1,13 @@
+const rows = prompt("your name in rows ");
+const column = prompt("your name in column");   
+
+const zia = [
+  ["zia", "khan", "irfan", "izzat"],
+  ["jabar", "ubaid", "kamran", "raheem"],
+  ["khalli", "naeem", "waqar", "junid"],
+];
+document.write(zia[rows][column]);
+
+// for (a = 0; a <= ka; a++) {
+//   document.write((zia = [1]));
+// }
