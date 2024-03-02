36246559354e65fd93c5250f7787c0c87bd5a6c0
ziauddin-0987 <ziauddink100@gmail.com>
2024-03-02 22:43:05 +0500

    this commit is a change array

--- my_file-in.js
+++ my_file-in.js
@@ -1,13 +1,30 @@
-const rows = prompt("your name in rows ");
-const column = prompt("your name in column");   
+// const rows = prompt("your name in rows ");
+// const column = prompt("your name in column");
 
-const zia = [
-  ["zia", "khan", "irfan", "izzat"],
-  ["jabar", "ubaid", "kamran", "raheem"],
-  ["khalli", "naeem", "waqar", "junid"],
-];
-document.write(zia[rows][column]);
+// let zia = [
+//   ["zia", "khan", "irfan", "izzat"],
+//   ["jabar", "ubaid", "kamran", "raheem"],
+//   ["khalli", "naeem", "waqar", "junid"],
+// ];
+// let ab = zia[0];
+// for (zia = 0; zia < ab; zia++) {
+//   document.write(zia);
+// }
+// console.log(zia[rows][column]);
 
 // for (a = 0; a <= ka; a++) {
 //   document.write((zia = [1]));
 // }
+let a = prompt("print in you favorits name'column'");
+let b = prompt("print in you favorits name'rows'");
+
+let z = [
+  ["ziauddin", "khan", "jumshad", "farhan"],
+  ["naeem", "noman", "tanweer", "izzat"],
+  ["ahamed", "muhammad", "usama", "salahuddin"],
+];
+for (i = 0; i <= z[a]; i++) {
+  console.log(z[a][b]);
+}
+console.log(z[a][b]);
+// console.log()
