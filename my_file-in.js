d306bc04c7c85a28e5424572ec28fa280c4cab60
ziauddin-0987 <ziauddink100@gmail.com>
2024-03-05 00:17:31 +0500

    this is js code prictice

--- my_file-in.js
+++ my_file-in.js
@@ -1,20 +1,20 @@
-// const rows = prompt("your name in rows ");
-// const column = prompt("your name in column");
+const rows = prompt("your name in rows ");
+const column = prompt("your name in column");
 
-// let zia = [
-//   ["zia", "khan", "irfan", "izzat"],
-//   ["jabar", "ubaid", "kamran", "raheem"],
-//   ["khalli", "naeem", "waqar", "junid"],
-// ];
-// let ab = zia[0];
-// for (zia = 0; zia < ab; zia++) {
-//   document.write(zia);
-// }
-// console.log(zia[rows][column]);
+let zia = [
+  ["zia", "khan", "irfan", "izzat"],
+  ["jabar", "ubaid", "kamran", "raheem"],
+  ["khalli", "naeem", "waqar", "junid"],
+];
+let ab = zia[0];
+for (zia = 0; zia < ab; zia++) {
+  document.write(zia);
+}
+console.log(zia[rows][column]);
 
-// for (a = 0; a <= ka; a++) {
-//   document.write((zia = [1]));
-// }
+for (a = 0; a <= ka; a++) {
+  document.write((zia = [1]));
+}
 let a = prompt("print in you favorits name'column'");
 let b = prompt("print in you favorits name'rows'");
 
@@ -27,4 +27,11 @@ for (i = 0; i <= z[a]; i++) {
   console.log(z[a][b]);
 }
 console.log(z[a][b]);
-// console.log()
+console.log();
+let e = prompt("odd and even");
+if (e % 2 == 0) {
+  console.log("even");
+} else {
+  console.log("odd");
+}
+// let b = prompt("even")
---
+++ new.js
@@ -0,0 +1,14 @@
+// let a = prompt("priint you value");
+
+// // for (a = 0; a <= 10; a++) {
+// //   console.log("even");
+// // }
+// if (a % 2 == 0) {
+//   console.log("yas is value even");
+// } else {
+//   console.log("odd");
+// }
+let a = 10; 
+let B = 20; 
+let c = result;
+switch(new message)
