9c015888cd072855bb30a9358f27fffb7a24760c
ziauddin-0987 <ziauddink100@gmail.com>
2024-03-07 01:01:04 +0500

    the commit is a function and is else

---
+++ suffha_.js
@@ -0,0 +1,6 @@
+function produ() {
+  let aa = parseInt(prompt("print your value "));
+  if (aa % 2 == 0) {
+    console.log("this is a value even");
+  } else if (console.log("this is value is a odd"));
+}
