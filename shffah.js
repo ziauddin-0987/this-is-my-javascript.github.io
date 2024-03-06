18db320744e0a7ae836841911b201274169bc9e7
ziauddin-0987 <ziauddink100@gmail.com>
2024-03-07 00:28:00 +0500

    this is  commit s function and divide

---
+++ shffah.js
@@ -0,0 +1,9 @@
+let divide;
+
+function divi() {
+  let a = parseInt(prompt("print your value divide"));
+  let b = parseInt(prompt("print your value divide"));
+  let c = a / b;
+  console.log(c);
+}
+// console.log(d + b);
