79d6a7c1107325f33e1ba58f2b63d140d78b04e6
ziauddin-0987 <ziauddink100@gmail.com>
2024-03-06 07:34:30 +0500

    this is switch condition 

--- new.js
+++ new.js
@@ -1,14 +1,48 @@
-// let a = prompt("priint you value");
+let a = prompt("priint you value");
 
-// // for (a = 0; a <= 10; a++) {
-// //   console.log("even");
-// // }
-// if (a % 2 == 0) {
-//   console.log("yas is value even");
-// } else {
-//   console.log("odd");
-// }
-let a = 10; 
-let B = 20; 
-let c = result;
-switch(new message)
+for (a = 0; a <= 10; a++) {
+  console.log("even");
+}
+if (a % 2 == 0) {
+  console.log("yas is value even");
+} else {
+  console.log("odd");
+}
+let day = prompt("print your value");
+
+switch (day) {
+  case 1:
+    console.log("monday");
+    break;
+  case 2:
+    console.log("tuesday");
+    break;
+  case 3:
+    console.log("wadnesday");
+    break;
+  case 4:
+    console.log("tursday");
+    break;
+  case 5:
+    console.log("friday");
+    break;
+  case 6:
+    console.log("sturday");
+    break;
+  case 7:
+    console.log("sunday");
+    break;
+}
+const b = [1, 3, 25, 6, 7, 5];
+for (const zia of b) {
+  console.log(zia);
+}
+const arr = ["zia", "khan", "rabi", "ubaid"];
+for (let arrays of arr) {
+  console.log(`this is my freands ${arrays}`);
+}
+let i;
+const neww = ["ziauddin", "izzat", "ubaid", "shahzad", "asif", "danyal"];
+for (i = 0; i < neww.length; i++) {
+  console.log(`this is my best freinde ${neww[i]}`);
+}
