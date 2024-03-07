c905f74c906806ca1c03af4bf3cc44e75f21a4b4
ziauddin-0987 <ziauddink100@gmail.com>
2024-03-08 01:17:19 +0500

    create a new js file but function and used

--- suffha_.js
+++ suffha_.js
@@ -1,6 +1,27 @@
-function produ() {
-  let aa = parseInt(prompt("print your value "));
-  if (aa % 2 == 0) {
-    console.log("this is a value even");
-  } else if (console.log("this is value is a odd"));
+// function produ() {
+//   let aa = parseInt(prompt("print your value "));
+//   if (aa % 2 == 0) {
+//     console.log("this is a value even");
+//   } else if (console.log("this is value is a odd"));
+// }
+function fun_name() {
+  let a = parseInt(prompt("print your numbe odd and even"));
+  if (a % 2 == 0) {
+    document.write("this is valided number 'even'");
+  } else if (a % 1 == 0) {
+    document.write("this is value a 'odd");
+  } else {
+    document.write("value is not valided");
+  }
+}
+
+function func_() {
+  const divide = parseInt(prompt("print your value"));
+  const div = parseInt(prompt("print your value so divide "));
+  const abc = divide / div;
+  console.log(abc);
+}
+let ab = prompt("koch be");
+for (i = 1; i < ab; ++a) {
+  console.log("ziauddin");
 }
